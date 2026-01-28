532e9b3bfe916ebe5bb69b592bdb908e381cefeb
isabelle <isabelle901@proton.me>
2026-01-27 18:37:04 -0800

    done!

--- src/main/java/org/ironriders/core/RobotContainer.java
+++ src/main/java/org/ironriders/core/RobotContainer.java
@@ -30,104 +30,112 @@ import edu.wpi.first.wpilibj2.command.button.CommandJoystick;
 import edu.wpi.first.wpilibj2.command.button.CommandXboxController;
 
 /**
- * Different button configurations for the driver controls PRIMARY_DRIVER: same as Driver Centered
- * Control Layout in the doc PRIMARY_DRIVER_WITH_BOOST: same as `William Boost buttons + primary
- * focus` in the doc SECONDARY_DRIVER_WITH_BOOST: same as `Secondary driver elevator controls` in
+ * Different button configurations for the driver controls PRIMARY_DRIVER: same
+ * as Driver Centered
+ * Control Layout in the doc PRIMARY_DRIVER_WITH_BOOST: same as `William Boost
+ * buttons + primary
+ * focus` in the doc SECONDARY_DRIVER_WITH_BOOST: same as `Secondary driver
+ * elevator controls` in
  * the doc.
  */
 enum Config {
-    PRIMARY_DRIVER, PRIMARY_DRIVER_WITH_BOOST, SECONDARY_DRIVER_WITH_BOOST;
+        PRIMARY_DRIVER, PRIMARY_DRIVER_WITH_BOOST, SECONDARY_DRIVER_WITH_BOOST;
 }
 
-
 /**
- * This class is where the bulk of the robot should be declared. Since Command-based is a
- * "declarative" paradigm, very little robot logic should actually be handled in the {@link Robot}
- * periodic methods (other than the scheduler calls). Instead, the structure of the robot (including
+ * This class is where the bulk of the robot should be declared. Since
+ * Command-based is a
+ * "declarative" paradigm, very little robot logic should actually be handled in
+ * the {@link Robot}
+ * periodic methods (other than the scheduler calls). Instead, the structure of
+ * the robot (including
  * subsystems, commands, and trigger mappings) should be declared here.
  */
 public class RobotContainer {
-    // The robot's subsystems and commands are defined here...
-    public final DriveSubsystem driveSubsystem = new DriveSubsystem();
-    public final DriveCommands driveCommands = driveSubsystem.getCommands();
+        // The robot's subsystems and commands are defined here...
+        public final DriveSubsystem driveSubsystem = new DriveSubsystem();
+        public final DriveCommands driveCommands = driveSubsystem.getCommands();
 
-    public final IndexerSubsystem indexerSubsystem = new IndexerSubsystem();
-    public final IndexerCommands indexerCommands = indexerSubsystem.getCommands();
+        public final IndexerSubsystem indexerSubsystem = new IndexerSubsystem();
+        public final IndexerCommands indexerCommands = indexerSubsystem.getCommands();
 
-    public final IntakeSubsystem intakeSubsystem = new IntakeSubsystem();
-    public final IntakeCommands intakeCommands = intakeSubsystem.getCommands();
+        public final IntakeSubsystem intakeSubsystem = new IntakeSubsystem();
+        public final IntakeCommands intakeCommands = intakeSubsystem.getCommands();
 
-    public final ShooterSubsystem shooterSubsystem = new ShooterSubsystem();
-    public final ShooterCommands shooterCommands = shooterSubsystem.getCommands();
+        public final ShooterSubsystem shooterSubsystem = new ShooterSubsystem();
+        public final ShooterCommands shooterCommands = shooterSubsystem.getCommands();
 
-    public final WristSubsystem wristSubsystem = new WristSubsystem();
-    public final WristCommands wristCommands = wristSubsystem.getCommands();
+        public final WristSubsystem wristSubsystem = new WristSubsystem();
+        public final WristCommands wristCommands = wristSubsystem.getCommands();
 
-    public final ClimberSubsystem climberSubsystem = new ClimberSubsystem();
-    public final ClimberCommands climberCommands = climberSubsystem.getCommands();
+        public final ClimberSubsystem climberSubsystem = new ClimberSubsystem();
+        public final ClimberCommands climberCommands = climberSubsystem.getCommands();
 
-    public final Double triggerThreshold = 0.75;
+        public final Double triggerThreshold = 0.75;
 
-    private final SendableChooser<Command> autoChooser;
+        private final SendableChooser<Command> autoChooser;
 
-    private final CommandXboxController primaryController =
-            new CommandXboxController(DriveConstants.PRIMARY_CONTROLLER_PORT);
-    private final CommandGenericHID secondaryController =
-            new CommandJoystick(DriveConstants.KEYPAD_CONTROLLER_PORT);
+        private final CommandXboxController primaryController = new CommandXboxController(
+                        DriveConstants.PRIMARY_CONTROLLER_PORT);
+        private final CommandGenericHID secondaryController = new CommandJoystick(
+                        DriveConstants.KEYPAD_CONTROLLER_PORT);
 
-    public final RobotCommands robotCommands =
-            new RobotCommands(driveCommands, primaryController.getHID());
+        public final RobotCommands robotCommands = new RobotCommands(driveCommands, indexerCommands, intakeCommands,
+                        shooterCommands, wristCommands, climberCommands, primaryController.getHID());
 
-    /**
-     * The container for the robot. Contains subsystems, IO devices, and commands.
-     *
-     * <p>
-     * builds the autos using {@link com.pathplanner.lib.auto.AutoBuilder#buildAutoChooser()
-     * buildAutoChooser()} posts the auto selection to
-     * {@link SmartDashboard#putData(String, SendableChooser) SmartDashboard}
-     */
-    public RobotContainer() {
-        // Configure the trigger bindings
-        configureBindings();
+        /**
+         * The container for the robot. Contains subsystems, IO devices, and commands.
+         *
+         * <p>
+         * builds the autos using
+         * {@link com.pathplanner.lib.auto.AutoBuilder#buildAutoChooser()
+         * buildAutoChooser()} posts the auto selection to
+         * {@link SmartDashboard#putData(String, SendableChooser) SmartDashboard}
+         */
+        public RobotContainer() {
+                // Configure the trigger bindings
+                configureBindings();
 
-        autoChooser = AutoBuilder.buildAutoChooser();
-        SmartDashboard.putData("Auto Select", autoChooser);
-    }
+                autoChooser = AutoBuilder.buildAutoChooser();
+                SmartDashboard.putData("Auto Select", autoChooser);
+        }
 
-    /**
-     * Th {@link CommandGenericHID#button(int)} method (such as
-     * {@link CommandXboxController#button(int)}, {@link CommandJoystick#button(int)}, or one of the
-     * {@link edu.wpi.first.wpilibj2.command.button.Trigger#Trigger(java.util.function.BooleanSupplier)}
-     * constructors.
-     */
-    private void configureBindings() {
-        DriverStation.silenceJoystickConnectionWarning(true);
+        /**
+         * Th {@link CommandGenericHID#button(int)} method (such as
+         * {@link CommandXboxController#button(int)},
+         * {@link CommandJoystick#button(int)}, or one of the
+         * {@link edu.wpi.first.wpilibj2.command.button.Trigger#Trigger(java.util.function.BooleanSupplier)}
+         * constructors.
+         */
+        private void configureBindings() {
+                DriverStation.silenceJoystickConnectionWarning(true);
 
-        // --- DRIVE CONTROLS ---
-        driveSubsystem.setDefaultCommand(robotCommands.driveTeleop(
-                () -> RobotUtils.controlCurve(-primaryController.getLeftY()
-                        * driveSubsystem.controlSpeedMultipler,
-                        DriveConstants.TRANSLATION_CONTROL_EXPONENT,
-                        DriveConstants.TRANSLATION_CONTROL_DEADBAND), // Deadband is unused currently
-                () -> RobotUtils.controlCurve(-primaryController.getLeftX()
-                        * driveSubsystem.controlSpeedMultipler,
-                        DriveConstants.TRANSLATION_CONTROL_EXPONENT,
-                        DriveConstants.TRANSLATION_CONTROL_DEADBAND),
-                () -> RobotUtils.controlCurve(-primaryController.getRightX()
-                        * driveSubsystem.controlSpeedMultipler,
-                        DriveConstants.ROTATION_CONTROL_EXPONENT,
-                        DriveConstants.ROTATION_CONTROL_DEADBAND)));
+                // --- DRIVE CONTROLS ---
+                driveSubsystem.setDefaultCommand(robotCommands.driveTeleop(
+                                () -> RobotUtils.controlCurve(-primaryController.getLeftY()
+                                                * driveSubsystem.controlSpeedMultipler,
+                                                DriveConstants.TRANSLATION_CONTROL_EXPONENT,
+                                                DriveConstants.TRANSLATION_CONTROL_DEADBAND), // Deadband is unused
+                                                                                              // currently
+                                () -> RobotUtils.controlCurve(-primaryController.getLeftX()
+                                                * driveSubsystem.controlSpeedMultipler,
+                                                DriveConstants.TRANSLATION_CONTROL_EXPONENT,
+                                                DriveConstants.TRANSLATION_CONTROL_DEADBAND),
+                                () -> RobotUtils.controlCurve(-primaryController.getRightX()
+                                                * driveSubsystem.controlSpeedMultipler,
+                                                DriveConstants.ROTATION_CONTROL_EXPONENT,
+                                                DriveConstants.ROTATION_CONTROL_DEADBAND)));
 
-        // --- OTHER CONTROLS ---
-        // TODO: Schedule a meeting to talk to drive about this once we have the design.
-    }
+                // --- OTHER CONTROLS ---
+                // TODO: Schedule a meeting to talk to drive about this once we have the design.
+        }
 
-    /**
-     * Get command configured in auto chooser.
-     *
-     * @return the command to run in autonomous
-     */
-    public Command getAutonomousCommand() {
-        return autoChooser.getSelected();
-    }
+        /**
+         * Get command configured in auto chooser.
+         *
+         * @return the command to run in autonomous
+         */
+        public Command getAutonomousCommand() {
+                return autoChooser.getSelected();
+        }
 }
