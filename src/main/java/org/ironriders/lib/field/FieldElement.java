002f4ebf07844933de7d06a11faa76de22bce51d
iprobablydontknowwhatimdoing <octact82@gmail.com>
2026-01-28 14:35:12 -0800

    Refactor FieldElement class to update element type s and positions for rebuilt version

--- src/main/java/org/ironriders/lib/field/FieldElement.java
+++ src/main/java/org/ironriders/lib/field/FieldElement.java
@@ -5,7 +5,9 @@ import edu.wpi.first.math.geometry.Pose3d;
 import edu.wpi.first.wpilibj.DriverStation;
 import java.util.ArrayList;
 import java.util.Collection;
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
 import java.util.Optional;
 import java.util.stream.Stream;
 
@@ -13,63 +15,99 @@ import java.util.stream.Stream;
 
 /**
  * Representation of an element on the field.
- *
- * <p>Includes pose, function, and various utility routines for retrieving.
+ * <hr />
+ * Includes:
+ * <ul>
+ * <li>Position</li>
+ * <li>Function</li>
+ * <li>Utility routines for retrieving.</li>
+ * </ul>
  */
 public class FieldElement {
 
   /** The type of elements on the field. */
   public enum ElementType {
-    STATION,
-    REEF,
-    PROCESSOR,
-    BARGE,
+    HUB,
+    BUMP,
+    TRENCH,
+    TOWER,
+    OUTPOST,
+    DEPOT;
   }
 
-  /** Generic (alliance-independent) element identifiers. */
+  /**
+   * Generic (alliance-independent) game element identifiers. There should be 1
+   * for each place with an april tag
+   * <hr />
+   * Each of these has an ID and an
+   * {@linkplain org.ironriders.lib.field.FieldElement.ElementType ElementType}.
+   */
   public enum Position {
-    LEFT_STATION(0, ElementType.STATION),
-    RIGHT_STATION(1, ElementType.STATION),
-    REEF_FRONT(2, ElementType.REEF),
-    REEF_FRONT_LEFT(3, ElementType.REEF),
-    REEF_BACK_LEFT(4, ElementType.REEF),
-    REEF_BACK(5, ElementType.REEF),
-    REEF_BACK_RIGHT(6, ElementType.REEF),
-    REEF_FRONT_RIGHT(7, ElementType.REEF),
-    PROCESSOR(8, ElementType.PROCESSOR),
-    BARGE(9, ElementType.BARGE);
+    OUTPOST(0, ElementType.OUTPOST),
+    DEPOT(1, ElementType.DEPOT),
+    TOWER_OUTPOST_SIDE(2, ElementType.TOWER),
+    TOWER_DEPOT_SIDE(3, ElementType.TOWER),
+    TOWER_CENTER(4, ElementType.TOWER),
+    BUMP_OUTPOST_SIDE(5, ElementType.BUMP),
+    BUMP_DEPOT_SIDE(6, ElementType.BUMP),
+    TRENCH_OUTPOST_SIDE(7, ElementType.TRENCH),
+    TRENCH_DEPOT_SIDE(8, ElementType.TRENCH);
 
-    public final int id;
-    public final ElementType type;
+    public final int ID;
+    public final ElementType TYPE;
 
     Position(int id, ElementType type) {
-      this.id = id;
-      this.type = type;
+      this.ID = id;
+      this.TYPE = type;
     }
   }
 
   public final Position position;
-  public final ElementType type;
+  public final ElementType TYPE;
   public final Pose3d pose;
   public final String name;
 
-  private static int[] BLUE_TAGS = {13, 12, 18, 19, 20, 21, 22, 17, 16, 14};
+  // TODO: update element tag ID's for rebuilt
+  private static HashMap<Position, Integer> BLUE_TAG_POSITIONS = new HashMap<>(Map.of(
+      Position.OUTPOST, 0,
+      Position.DEPOT, 0,
+      Position.TOWER_OUTPOST_SIDE, 0,
+      Position.TOWER_DEPOT_SIDE, 0,
+      Position.TOWER_CENTER, 0,
+      Position.BUMP_OUTPOST_SIDE, 0,
+      Position.BUMP_DEPOT_SIDE, 0,
+      Position.TRENCH_OUTPOST_SIDE, 0,
+      Position.TRENCH_DEPOT_SIDE, 0)); // Integer is the April Tag ID for that position
+  private static HashMap<Position, Integer> RED_TAG_POSITIONS = new HashMap<>(Map.of(
+      Position.OUTPOST, 0,
+      Position.DEPOT, 0,
+      Position.TOWER_OUTPOST_SIDE, 0,
+      Position.TOWER_DEPOT_SIDE, 0,
+      Position.TOWER_CENTER, 0,
+      Position.BUMP_OUTPOST_SIDE, 0,
+      Position.BUMP_DEPOT_SIDE, 0,
+      Position.TRENCH_OUTPOST_SIDE, 0,
+      Position.TRENCH_DEPOT_SIDE, 0)); // Integer is the April Tag ID for that position
 
-  private static int[] RED_TAGS = {1, 2, 7, 6, 11, 10, 9, 8, 3, 5};
-
-  private static List<FieldElement> BLUE_ELEMENTS =
-      loadElements(DriverStation.Alliance.Blue, BLUE_TAGS);
-  private static List<FieldElement> RED_ELEMENTS =
-      loadElements(DriverStation.Alliance.Red, RED_TAGS);
+  private static List<FieldElement> BLUE_ELEMENTS = loadElements(DriverStation.Alliance.Blue, BLUE_TAG_POSITIONS);
+  private static List<FieldElement> RED_ELEMENTS = loadElements(DriverStation.Alliance.Red, RED_TAG_POSITIONS);
 
   private FieldElement(Position element, Pose3d pose) {
     this.position = element;
-    this.type = element.type;
+    this.TYPE = element.TYPE;
     this.name = element.name();
     this.pose = pose;
   }
 
-  /** Retrieve all elements for an alliance. */
+  /**
+   * Retrieve all elements for an alliance.
+   * 
+   * @param alliance The alliance to retrieve elements for. If this is empty, will
+   *                 return an empty list.
+   * @return A collection of field elements for the given alliance. Uses
+   *         {@linkplain org.ironriders.lib.field.FieldElement#loadElements(edu.wpi.first.wpilibj.DriverStation.Alliance, int[])
+   *         loadElements} to generate.
+   */
   public static Collection<FieldElement> of(Optional<DriverStation.Alliance> alliance) {
     if (alliance.isEmpty()) {
       return new ArrayList<FieldElement>();
@@ -92,7 +130,15 @@ public class FieldElement {
     return Optional.empty();
   }
 
-  /** Retrieve the closest alliance element of a desired type. */
+  /**
+   * Retrieve the closest alliance element of a desired type.
+   * 
+   * @param pose the position to check from
+   * @param type the type of element to find, from
+   *             {@linkplain org.ironriders.lib.field.FieldElement.ElementType
+   *             ElementType} enum
+   * @return the nearest element of the given type to the given position
+   */
   public static Optional<FieldElement> nearestTo(Pose2d pose, ElementType type) {
     return findNearest(pose, Optional.of(type));
   }
@@ -102,11 +148,22 @@ public class FieldElement {
     return findNearest(pose, Optional.empty());
   }
 
-  private static List<FieldElement> loadElements(DriverStation.Alliance alliance, int[] tags) {
+  /**
+   * Load elements for an alliance from their AprilTag IDs.
+   * 
+   * @param alliance the {@linkplain edu.wpi.first.wpilibj.DriverStation.Alliance
+   *                 Alliance} to load for
+   * @param tags     A map of
+   *                 {@linkplain org.ironriders.lib.field.FieldElement.Position
+   *                 Position's}, and the value is the AprilTag at that position
+   *                 Position
+   * @return a list of loaded field elements
+   */
+  private static List<FieldElement> loadElements(DriverStation.Alliance alliance, HashMap<Position, Integer> tags) {
     return Stream.of(Position.values())
         .map(
             element -> {
-              var pose = FieldUtils.FIELD_LAYOUT.getTagPose(tags[element.id]);
+              var pose = FieldUtils.FIELD_LAYOUT.getTagPose(tags.get(element));
               if (pose.isEmpty()) {
                 Optional.empty();
               }
@@ -122,13 +179,13 @@ public class FieldElement {
     Optional<FieldElement> found = Optional.empty();
 
     for (var element : of(DriverStation.getAlliance())) {
-      if (type.isPresent() && element.type != type.get()) {
+      if (type.isPresent() && element.TYPE != type.get()) { // Skips the element if its not the right type
         continue;
       }
 
-      double thisDistance =
-          pose.getTranslation().getDistance(element.pose.toPose2d().getTranslation());
-      if (found.isEmpty() || distance > thisDistance) {
+      double thisDistance = pose.getTranslation().getDistance(element.pose.toPose2d().getTranslation());
+      if (found.isEmpty() || distance > thisDistance) { // if its the first itoration and found hasn't been set, will
+                                                        // set found to the first element
         distance = thisDistance;
         found = Optional.of(element);
       }
